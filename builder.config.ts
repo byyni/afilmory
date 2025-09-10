affe9d0bc64622d528148ea738709844b652aed5
tanmantang <1059767677@qq.com>
2025-09-11 00:46:33 +0800

    测试取消useClusterMode

--- builder.config.ts
+++ builder.config.ts
@@ -113,7 +113,7 @@ export const defaultBuilderConfig: BuilderConfig = {
     worker: {
       workerCount: os.cpus().length * 2,
       timeout: 30000, // 30 seconds
-      useClusterMode: true,
+      useClusterMode: false,
       workerConcurrency: 2,
     },
   },
